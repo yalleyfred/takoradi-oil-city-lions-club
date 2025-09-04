587221a2347c2e5805cf77cd00d12774e070a3b2
Evans Elabo <ellaboevans@gmail.com>
2025-09-04 16:35:18 +0000

    style(what-we-do): improve code formatting for consistency and readability

--- src/app/(landing)/_components/what-we-do.tsx
+++ src/app/(landing)/_components/what-we-do.tsx
@@ -14,16 +14,14 @@ export default function WhatWeDo() {
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8, ease: "easeOut" }}
       viewport={{ once: true, amount: 0.3 }}
-      className="py-24 px-4 flex items-center bg-slate-100 justify-start gap-16 my-10"
-    >
+      className="py-24 px-4 flex items-center bg-slate-100 justify-start gap-16 my-10">
       <div className="w-6xl mx-auto flex flex-col lg:flex-row-reverse items-center gap-10 justify-start">
         <motion.div
           initial={{ opacity: 0, x: 40 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.8, delay: 0.2 }}
           viewport={{ once: true, amount: 0.3 }}
-          className="flex-col"
-        >
+          className="flex-col">
           <SectionHeader title="What We Do" />
           <h1 className="text-2xl lg:text-5xl text-center lg:text-left mt-4 pr-3">
             Transforming Lives Across Ghana
@@ -42,8 +40,7 @@ export default function WhatWeDo() {
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.6, delay: 0.3 + service.id! * 0.1 }}
                 viewport={{ once: true, amount: 0.3 }}
-                className="flex items-center gap-1 py-2"
-              >
+                className="flex items-center gap-1 py-2">
                 <Service
                   title={service.title}
                   description={service.description}
@@ -52,15 +49,12 @@ export default function WhatWeDo() {
             ))}
           </div>
         </motion.div>
-
-        {/* Image Column */}
         <motion.div
           initial={{ opacity: 0, x: -40 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.8, delay: 0.2 }}
           viewport={{ once: true, amount: 0.3 }}
-          className="flex items-center w-full lg:w-[40rem] lg:h-[35rem] bg-slate-800/40 rounded-lg"
-        >
+          className="flex items-center w-full lg:w-[40rem] lg:h-[35rem] bg-slate-800/40 rounded-lg">
           <Image
             src="https://images.unsplash.com/photo-1655720359248-eeace8c709c5?q=80&w=2125&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="image"
