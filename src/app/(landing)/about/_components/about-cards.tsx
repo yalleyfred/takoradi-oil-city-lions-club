3b74561becfee6d034af2ec4b524334d151f104d
Evans Elabo <ellaboevans@gmail.com>
2025-09-04 16:34:59 +0000

    style(about-cards): improve code formatting for consistency and readability

--- src/app/(landing)/about/_components/about-cards.tsx
+++ src/app/(landing)/about/_components/about-cards.tsx
@@ -2,19 +2,17 @@ import { AboutCard } from "@/types/card.interface";
 import Link from "next/link";
 
 export default function AboutCards({ data }: { readonly data: AboutCard }) {
-	return (
-		<div
-			className={`${data.color} rounded-2xl p-8 text-white flex flex-col gap-4 h-full justify-between`}
-		>
-			<Link
-				href="/"
-				className="self-end px-4 py-2 border border-white rounded-full hover:bg-white hover:text-blue-900 transition-all text-right"
-			>
-				{data.buttonText}
-			</Link>
+  return (
+    <div
+      className={`${data.color} rounded-2xl p-8 text-white flex flex-col gap-4 h-full justify-between`}>
+      <Link
+        href="/"
+        className="self-end px-4 py-2 border border-white rounded-full hover:bg-white hover:text-blue-900 transition-all text-right">
+        {data.buttonText}
+      </Link>
 
-			<h4 className="text-xl font-bold">{data.title}</h4>
-			<p className="leading-relaxed">{data.description}</p>
-		</div>
-	);
+      <h4 className="text-xl font-bold">{data.title}</h4>
+      <p className="leading-relaxed">{data.description}</p>
+    </div>
+  );
 }
