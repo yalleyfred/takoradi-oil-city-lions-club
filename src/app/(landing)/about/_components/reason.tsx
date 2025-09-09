14d2a5ae022846f458464ef08507452e2770840c
Michael Darko <michael.darko@amalitech.com>
2025-09-09 15:35:03 +0000

    refactor(reason): change template literal to cn method

--- src/app/(landing)/about/_components/reason.tsx
+++ src/app/(landing)/about/_components/reason.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import { WhyJoinUs } from "@/types/reason.interface";
 
 export default function Reason({ data }: { readonly data: WhyJoinUs }) {
@@ -6,7 +7,7 @@ export default function Reason({ data }: { readonly data: WhyJoinUs }) {
   return (
     <li className="flex flex-col py-6 md:px-6 text-center md:text-left">
       <div className="flex flex-col md:flex-row items-center gap-4 mb-4">
-        <Icon className={`w-8 h-8 ${data.color}`} />
+        <Icon className={cn("w-8 h-8", data.color)} />
         <h3 className="text-2xl font-bold">{data.title}</h3>
       </div>
       <p>{data.description}</p>
