c16eb3822c951bfbee3a03272224fc5a3bca094f
Evans Elabo <ellaboevans@gmail.com>
2025-09-10 12:14:49 +0000

    fix(nav-links): update path for "What We Do" to "Team"

--- src/constants/nav-links.ts
+++ src/constants/nav-links.ts
@@ -1,7 +1,7 @@
 export const NavLinks = [
-	{ id: 1, path: "/about", label: "About Us" },
-	{ id: 2, path: "/programs", label: "Our Programs" },
-	{ id: 3, path: "/events", label: "Events" },
-	{ id: 5, path: "/contact", label: "Contact Us" },
-	{ id: 6, path: "/what-we-do", label: "What We Do" },
+  { id: 1, path: "/about", label: "About Us" },
+  { id: 2, path: "/programs", label: "Our Programs" },
+  { id: 3, path: "/events", label: "Events" },
+  { id: 5, path: "/contact", label: "Contact Us" },
+  { id: 6, path: "/team", label: "Team" },
 ];
