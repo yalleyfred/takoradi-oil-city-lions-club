a8ed5842a8e0f18c889ea2bc9d82685f340fb206
Evans Elabo <ellaboevans@gmail.com>
2025-09-13 13:04:22 +0000

    feat(components): refactor EventSidebar, Event, Hero, Testimonials, and UpcomingPrograms for improved readability and consistency

--- src/app/(landing)/programs/_components/event-sidebar.tsx
+++ src/app/(landing)/programs/_components/event-sidebar.tsx
@@ -5,87 +5,86 @@ import { motion } from "motion/react";
 import React from "react";
 
 interface EventSidebarProps {
-	readonly setIsPopUpOpen: React.Dispatch<React.SetStateAction<boolean>>;
-	readonly programs: readonly UpcomingEvent[];
-	readonly currentIndex: number;
+  readonly setIsPopUpOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  readonly programs: readonly UpcomingEvent[];
+  readonly currentIndex: number;
 }
 
 export function EventSidebar({
-	setIsPopUpOpen,
-	programs,
-	currentIndex,
+  setIsPopUpOpen,
+  programs,
+  currentIndex,
 }: EventSidebarProps) {
-	return (
-		<>
-			<motion.div
-				className="fixed top-0 right-0 h-screen w-screen bg-blue-900/20 z-40 overflow-y-auto"
-				initial={{ opacity: 0 }}
-				animate={{ opacity: 1 }}
-				exit={{ opacity: 0 }}
-				transition={{ duration: 0.3 }}
-				onClick={() => setIsPopUpOpen(false)}
-			/>
+  return (
+    <>
+      <motion.div
+        className="fixed top-0 right-0 h-screen w-screen bg-blue-900/20 z-40 overflow-y-auto"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.3 }}
+        onClick={() => setIsPopUpOpen(false)}
+      />
 
-			<motion.div
-				className="h-full overflow-y-auto md:h-[calc(100vh-1.5rem)] w-full md:w-2/3 lg:w-2/5 2xl:w-1/3 text-white bg-blue-900 shadow-2xl flex flex-col items-center gap-10 fixed top-0 md:top-3 right-0 py-8 md:rounded-l-lg z-50"
-				initial={{ x: "100%" }}
-				animate={{ x: 0 }}
-				exit={{ x: "100%" }}
-				transition={{ type: "spring", stiffness: 80, damping: 15 }}
-			>
-				<X
-					className="w-8 h-8 hover:cursor-pointer absolute top-3 right-4"
-					onClick={() => setIsPopUpOpen(false)}
-				/>
-				<img
-					src={programs[currentIndex].image}
-					alt={programs[currentIndex].title}
-					className="w-4/5 md:w-3/5 lg:w-7/12 2xl:w-2/4 h-92 md:h-72 lg:h-80 object-cover rounded-md mt-10"
-				/>
+      <motion.div
+        className="h-full overflow-y-auto md:h-[calc(100vh-1.5rem)] w-full md:w-2/3 lg:w-2/5 2xl:w-1/3 text-white bg-blue-900 shadow-2xl flex flex-col items-center gap-10 fixed top-0 md:top-3 right-0 py-8 md:rounded-l-lg z-50"
+        initial={{ x: "100%" }}
+        animate={{ x: 0 }}
+        exit={{ x: "100%" }}
+        transition={{ type: "spring", stiffness: 80, damping: 15 }}>
+        <X
+          className="w-8 h-8 hover:cursor-pointer absolute top-3 right-4"
+          onClick={() => setIsPopUpOpen(false)}
+        />
+        <img
+          src={programs[currentIndex].image}
+          alt={programs[currentIndex].title}
+          className="w-4/5 md:w-3/5 lg:w-7/12 2xl:w-2/4 h-92 md:h-72 lg:h-80 object-cover rounded-md mt-10"
+        />
 
-				<div className="mr-auto px-10 flex flex-col gap-6">
-					<div className="flex flex-col gap-2">
-						<h2 className="text-2xl md:text-3xl font-bold">
-							{programs[currentIndex].title}
-						</h2>
-						<p className="text-xs md:text-sm">
-							{programs[currentIndex].description}
-						</p>
-					</div>
-					<div className="flex items-center gap-4">
-						<span className="w-12 h-12 bg-white rounded-xl overflow-hidden">
-							<div className="w-full h-2/5 bg-gray-700 text-center text-sm m-auto overflow-hidden">
-								{format(programs[currentIndex].date, "MMM")}
-							</div>
-							<div className="text-gray-700 font-bold text-lg text-center">
-								{format(programs[currentIndex].date, "dd")}
-							</div>
-						</span>
-						<div>
-							<p className="text-lg md:text-xl font-bold">
-								{format(programs[currentIndex].date, "EEEE do MMMM")}
-							</p>
-							<p className="text-sm md:text-base">
-								{programs[currentIndex].time}
-							</p>
-						</div>
-					</div>
-					<div className="flex items-center gap-4">
-						<LocationEdit className="w-12 h-12 text-white" />
-						<p className="text-lg md:text-xl font-bold">
-							{programs[currentIndex].location}
-						</p>
-					</div>
-				</div>
+        <div className="mr-auto px-10 flex flex-col gap-6">
+          <div className="flex flex-col gap-2">
+            <h2 className="text-2xl md:text-3xl font-bold">
+              {programs[currentIndex].title}
+            </h2>
+            <p className="text-xs md:text-sm">
+              {programs[currentIndex].description}
+            </p>
+          </div>
+          <div className="flex items-center gap-4">
+            <span className="w-12 h-12 bg-white rounded-xl overflow-hidden">
+              <div className="w-full h-2/5 bg-gray-700 text-center text-sm m-auto overflow-hidden">
+                {format(programs[currentIndex].date, "MMM")}
+              </div>
+              <div className="text-gray-700 font-bold text-lg text-center">
+                {format(programs[currentIndex].date, "dd")}
+              </div>
+            </span>
+            <div>
+              <p className="text-lg md:text-xl font-bold">
+                {format(programs[currentIndex].date, "EEEE do MMMM")}
+              </p>
+              <p className="text-sm md:text-base">
+                {programs[currentIndex].time}
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center gap-4">
+            <LocationEdit className="w-12 h-12 text-white" />
+            <p className="text-lg md:text-xl font-bold">
+              {programs[currentIndex].location}
+            </p>
+          </div>
+        </div>
 
-				<div className="mr-auto px-10 flex flex-col gap-2 w-full">
-					<h6 className="font-semibold">About Event</h6>
-					<hr />
-					<p className="text-sm md:text-base">
-						{programs[currentIndex].details}
-					</p>
-				</div>
-			</motion.div>
-		</>
-	);
+        <div className="mr-auto px-10 flex flex-col gap-2 w-full">
+          <h6 className="font-semibold">About Event</h6>
+          <hr />
+          <p className="text-sm md:text-base">
+            {programs[currentIndex].details}
+          </p>
+        </div>
+      </motion.div>
+    </>
+  );
 }
--- src/app/(landing)/programs/_components/testimonials.tsx
+++ src/app/(landing)/programs/_components/testimonials.tsx
@@ -5,127 +5,125 @@ import { useEffect, useState } from "react";
 import { motion } from "motion/react";
 
 export function Testimonials() {
-	const [currentGroupIndex, setCurrentGroupIndex] = useState(0);
-	const numberPerGroup = 6;
-	const groups = Math.ceil(testimonials.length / 6);
-	const [testimonialsData, setTestimonialsData] = useState(
-		testimonials.slice(
-			numberPerGroup * currentGroupIndex,
-			numberPerGroup * currentGroupIndex + numberPerGroup
-		)
-	);
+  const [currentGroupIndex, setCurrentGroupIndex] = useState(0);
+  const numberPerGroup = 6;
+  const groups = Math.ceil(testimonials.length / 6);
+  const [testimonialsData, setTestimonialsData] = useState(
+    testimonials.slice(
+      numberPerGroup * currentGroupIndex,
+      numberPerGroup * currentGroupIndex + numberPerGroup
+    )
+  );
 
-	useEffect(() => {
-		setTestimonialsData(
-			testimonials.slice(
-				numberPerGroup * currentGroupIndex,
-				numberPerGroup * currentGroupIndex + numberPerGroup
-			)
-		);
-	}, [currentGroupIndex]);
+  useEffect(() => {
+    setTestimonialsData(
+      testimonials.slice(
+        numberPerGroup * currentGroupIndex,
+        numberPerGroup * currentGroupIndex + numberPerGroup
+      )
+    );
+  }, [currentGroupIndex]);
 
-	useEffect(() => {
-		const handleKeyDown = (event: KeyboardEvent) => {
-			if (event.key === "ArrowRight") {
-				setCurrentGroupIndex((prev) => (prev + 1) % groups);
-			} else if (event.key === "ArrowLeft") {
-				setCurrentGroupIndex((prev) => (prev - 1 + groups) % groups);
-			}
-		};
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "ArrowRight") {
+        setCurrentGroupIndex((prev) => (prev + 1) % groups);
+      } else if (event.key === "ArrowLeft") {
+        setCurrentGroupIndex((prev) => (prev - 1 + groups) % groups);
+      }
+    };
 
-		window.addEventListener("keydown", handleKeyDown);
-		return () => window.removeEventListener("keydown", handleKeyDown);
-	}, [groups, setCurrentGroupIndex]);
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [groups, setCurrentGroupIndex]);
 
-	useEffect(() => {
-		const mediaQuery = window.matchMedia("(min-width: 768px)");
-		let interval: NodeJS.Timeout | null = null;
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(min-width: 768px)");
+    let interval: NodeJS.Timeout | null = null;
 
-		const updateGroupIndex = () => {
-			setCurrentGroupIndex((prev) => (prev + 1) % groups);
-		};
+    const updateGroupIndex = () => {
+      setCurrentGroupIndex((prev) => (prev + 1) % groups);
+    };
 
-		const startAutoSlide = () => {
-			interval = setInterval(updateGroupIndex, 10000);
-		};
+    const startAutoSlide = () => {
+      interval = setInterval(updateGroupIndex, 10000);
+    };
 
-		if (mediaQuery.matches) startAutoSlide();
+    if (mediaQuery.matches) startAutoSlide();
 
-		const handleChange = (e: MediaQueryListEvent) => {
-			if (e.matches) {
-				startAutoSlide();
-			} else if (interval) {
-				clearInterval(interval);
-				interval = null;
-			}
-		};
+    const handleChange = (e: MediaQueryListEvent) => {
+      if (e.matches) {
+        startAutoSlide();
+      } else if (interval) {
+        clearInterval(interval);
+        interval = null;
+      }
+    };
 
-		mediaQuery.addEventListener("change", handleChange);
+    mediaQuery.addEventListener("change", handleChange);
 
-		return () => {
-			if (interval) clearInterval(interval);
-			mediaQuery.removeEventListener("change", handleChange);
-		};
-	}, [groups]);
+    return () => {
+      if (interval) clearInterval(interval);
+      mediaQuery.removeEventListener("change", handleChange);
+    };
+  }, [groups]);
 
-	return (
-		<section className="flex flex-col py-10 gap-16 px-6 text-white justify-center items-center bg-center bg-cover bg-[url('https://images.unsplash.com/photo-1531844251246-9a1bfaae09fc?w=1600&auto=format&fit=crop&q=90')] filter grayscale-25">
-			<div className="text-center flex flex-col gap-3">
-				<h2 className="text-4xl lg:text-5xl font-bold">
-					Lives Touched, Futures Changed
-				</h2>
-				<p className="text-base lg:text-lg">
-					Every program plants a seed of hope — here are the stories that grew
-					from them
-				</p>
-			</div>
-			<div className="w-full min-h-[50vh] text-center py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-				{testimonialsData.map((testimonial, index) => (
-					<motion.div
-						key={testimonial.id}
-						className="flex flex-col gap-3 py-10 bg-blue-100/60 backdrop-blur-sm rounded-lg items-center justify-center text-blue-900 px-6 shadow-lg h-60 lg:h-76 2xl:h-68"
-						initial={{ opacity: 0, y: 40 }}
-						animate={{ opacity: 1, y: 0 }}
-						transition={{ duration: 0.5, delay: index * 0.1, ease: "easeOut" }}
-						whileHover={{ scale: 1.03 }}
-					>
-						<div className="flex items-center gap-6 text-left">
-							<img
-								src={testimonial.image}
-								alt={testimonial.name}
-								className="w-15 2xl:w-20 h-15 2xl:h-20 object-cover mb-4 mx-auto rounded-full"
-							/>
-							<div>
-								<h5 className="text-xl lg:text-2xl font-bold">
-									{testimonial.name}
-								</h5>
-								<h6>{testimonial.role}</h6>
-							</div>
-						</div>
-						<p className="text-sm lg:text-lg">"{testimonial.quote}"</p>
-					</motion.div>
-				))}
-			</div>
+  return (
+    <section className="flex flex-col py-10 gap-16 px-6 text-white justify-center items-center bg-center bg-cover bg-[url('https://images.unsplash.com/photo-1531844251246-9a1bfaae09fc?w=1600&auto=format&fit=crop&q=90')] filter grayscale-25">
+      <div className="text-center flex flex-col gap-3">
+        <h2 className="text-4xl lg:text-5xl font-bold">
+          Lives Touched, Futures Changed
+        </h2>
+        <p className="text-base lg:text-lg">
+          Every program plants a seed of hope — here are the stories that grew
+          from them
+        </p>
+      </div>
+      <div className="w-full min-h-[50vh] text-center py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+        {testimonialsData.map((testimonial, index) => (
+          <motion.div
+            key={testimonial.id}
+            className="flex flex-col gap-3 py-10 bg-blue-100/60 backdrop-blur-sm rounded-lg items-center justify-center text-blue-900 px-6 shadow-lg h-60 lg:h-76 2xl:h-68"
+            initial={{ opacity: 0, y: 40 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: index * 0.1, ease: "easeOut" }}
+            whileHover={{ scale: 1.03 }}>
+            <div className="flex items-center gap-6 text-left">
+              <img
+                src={testimonial.image}
+                alt={testimonial.name}
+                className="w-15 2xl:w-20 h-15 2xl:h-20 object-cover mb-4 mx-auto rounded-full"
+              />
+              <div>
+                <h5 className="text-xl lg:text-2xl font-bold">
+                  {testimonial.name}
+                </h5>
+                <h6>{testimonial.role}</h6>
+              </div>
+            </div>
+            <p className="text-sm lg:text-lg">"{testimonial.quote}"</p>
+          </motion.div>
+        ))}
+      </div>
 
-			<span className="flex gap-4">
-				{Array.from({ length: groups }).map((_, index) => (
-					<button
-						key={index + 1}
-						className="relative rounded-full w-4 h-4 cursor-pointer"
-						onClick={() => setCurrentGroupIndex(index)}
-					>
-						<span className="absolute inset-0 rounded-full bg-blue-900 opacity-30"></span>
+      <span className="flex gap-4">
+        {Array.from({ length: groups }).map((_, index) => (
+          <button
+            key={index + 1}
+            className="relative rounded-full w-4 h-4 cursor-pointer"
+            onClick={() => setCurrentGroupIndex(index)}>
+            <span className="absolute inset-0 rounded-full bg-blue-900 opacity-30"></span>
 
-						{index === currentGroupIndex && (
-							<motion.span
-								layoutId="activeIndicator"
-								className="absolute inset-0 rounded-full bg-blue-900"
-								transition={{ type: "spring", stiffness: 500, damping: 30 }}
-							/>
-						)}
-					</button>
-				))}
-			</span>
-		</section>
-	);
+            {index === currentGroupIndex && (
+              <motion.span
+                layoutId="activeIndicator"
+                className="absolute inset-0 rounded-full bg-blue-900"
+                transition={{ type: "spring", stiffness: 500, damping: 30 }}
+              />
+            )}
+          </button>
+        ))}
+      </span>
+    </section>
+  );
 }
