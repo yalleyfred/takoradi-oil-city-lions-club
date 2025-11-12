3f868c8ef64d0691bde0e398cefc64190c72abc4
Evans Elabo <ellaboevans@gmail.com>
2025-11-12 16:31:04 +0000

    Add smooth layout animations

--- src/app/(landing)/events/_components/event-list.tsx
+++ src/app/(landing)/events/_components/event-list.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { EventDrawer } from "./event-drawer";
 import { EventItem } from "@/types/event.interface";
 import { EventCard } from "./event-card";
@@ -35,21 +35,25 @@ export default function EventList({ data }: Readonly<Props>) {
             },
           },
         }}>
-        {data.map((ev, idx) => (
-          <motion.div
-            key={ev._id}
-            initial={{ opacity: 0, y: 40 }}
-            animate={{ opacity: 1, y: 0 }}
-            whileHover={{ scale: 1.03 }}
-            transition={{
-              type: "keyframes",
-              stiffness: 400,
-              damping: 30,
-              delay: idx * 0.1,
-            }}>
-            <EventCard event={ev} onClick={handleOpen} />
-          </motion.div>
-        ))}
+
+        <AnimatePresence mode="popLayout">
+          {data.map((ev, idx) => (
+            <motion.div
+              key={ev._id}
+              layout
+              initial={{ opacity: 0, y: 40 }}
+              animate={{ opacity: 1, y: 0 }}
+              whileHover={{ scale: 1.03 }}
+              transition={{
+                type: "keyframes",
+                stiffness: 400,
+                damping: 30,
+                delay: idx * 0.1,
+              }}>
+              <EventCard event={ev} onClick={handleOpen} />
+            </motion.div>
+          ))}
+        </AnimatePresence>
       </motion.div>
 
       <div className="absolute z-50">
--- src/app/(landing)/team/_components/team-list.tsx
+++ src/app/(landing)/team/_components/team-list.tsx
@@ -3,7 +3,7 @@
 import { TeamInterface } from "@/types/team.interface";
 import React from "react";
 import TeamCard from "./team-card";
-import { motion } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 
 type Props = {
   members: TeamInterface[];
@@ -12,17 +12,21 @@ type Props = {
 export default function TeamLists({ members }: Props) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-10 px-4">
-      {members.map((member, index) => (
-        <motion.div
-          key={member.id}
-          initial={{ opacity: 0, y: 20, scale: 0.98 }}
-          whileInView={{ opacity: 1, y: 0, scale: 1 }}
-          transition={{ duration: 0.6, delay: 0.2 + index * 0.15 }}
-          viewport={{ once: true, amount: 0.2 }}
-          className="w-full">
-          <TeamCard key={member.id} data={member} />
-        </motion.div>
-      ))}
+
+      <AnimatePresence mode="popLayout">
+        {members.map((member, index) => (
+          <motion.div
+            key={member.id}
+            layout
+            initial={{ opacity: 0, y: 20, scale: 0.98 }}
+            whileInView={{ opacity: 1, y: 0, scale: 1 }}
+            transition={{ duration: 0.6, delay: 0.2 + index * 0.15 }}
+            viewport={{ once: true, amount: 0.2 }}
+            className="w-full">
+            <TeamCard key={member.id} data={member} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 }
