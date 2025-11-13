c8bc90306b40c3ea13fe5c8e77446bc57059a826
Evans Elabo <ellaboevans@gmail.com>
2025-11-13 08:16:58 +0000

    feat(app): add mail transporter for emalis

--- src/utils/mail-transporter.ts
+++ src/utils/mail-transporter.ts
@@ -1,5 +1,4 @@
 import nodemailer from "nodemailer";
-import { google } from "googleapis";
 
 interface MailOptions {
   to: string;
@@ -7,37 +6,12 @@ interface MailOptions {
   message: string;
 }
 
-const oauth2Client = new google.auth.OAuth2(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_REDIRECT_URL
-);
-
-oauth2Client.setCredentials({
-  refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
-});
-
-async function getAccessToken() {
-  try {
-    const { token } = await oauth2Client.getAccessToken();
-    return token;
-  } catch (error) {
-    console.error("Failed to get access token:", error);
-    throw error;
-  }
-}
-
 export async function createTransporter() {
-  const accessToken = await getAccessToken();
-
   return nodemailer.createTransport({
     service: "gmail",
     auth: {
-      type: "OAuth2",
       user: process.env.GMAIL_USER,
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+      pass: process.env.GMAIL_PASSWORD,
     },
   });
 }
